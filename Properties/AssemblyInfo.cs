e12a5d3c1a55bfecaff8621e78ac6fde08068394
rk0exn <214826087+rk0exn@users.noreply.github.com>
2025-06-08 12:43:12 +0900

    Upload AssemblyInfo.cs

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+using System.Reflection;
+using System.Windows;
+[assembly: AssemblyTitle("ttvedit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ttvedit")]
+[assembly: AssemblyCopyright("Copyright (c) 2025 All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: System.Runtime.InteropServices.ComVisible(false)]
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: AssemblyVersion("0.9.1.1")]
+[assembly: AssemblyFileVersion("0.9.1.1")]
